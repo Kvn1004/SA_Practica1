524ad400aaa8fcdd989b47aa8c8ee8c1e016a58c
Kvn1004 <pex089@gmail.com>
2020-08-08 10:31:53 -0600

    Primer commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
+var token = function autenticar() {
+    $.ajax({
+        type: "POST",
+        crossDomain: true,
+        dataType: "json",
+        url: "https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal",
+        headers: {
+            "Accept": "application/json",
+            "Authorization": "Basic"
+        },
+
+        data: {
+            grant_type: "client_credentials",
+            client_id: "sa",
+            client_secret: "fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745",
+        },
+
+        success: function(response) {
+            console.log(response.access_token);
+        },
+        error: function(xhr, status, error) {
+            console.log(xhr + ", " + status + ", " + error);
+        }
+    })
+}
+
 //Función para obtener la lista de usuarios desde la API, usando AJAX
 function listaUsuarios() {
+    var test = token;
+    console.log(test);
     $.ajax({
         type: "GET",
         crossDomain: true,
