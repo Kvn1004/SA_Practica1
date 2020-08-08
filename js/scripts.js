752cf02acfdbdc9e03600a9c121e205bb92daf9e
Kvn1004 <pex089@gmail.com>
2020-08-07 23:26:42 -0600

    Listar usuarios

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,19 @@ function listaUsuarios() {
         crossDomain: true,
         url: "https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal",
         success: function(response) {
+            var tablaUsuarios = "";
             var datos = response._embedded.item;
             console.log('callback success: ', datos);
+            var tabla = document.createElement('table');
             for (var i = 0; i < datos.length; i++) {
-                console.log(datos[i].name);
+                var tr = document.createElement('tr');
+                var td = document.createElement('td');
+                var contc = document.createTextNode(datos[i].name);
+                td.appendChild(contc);
+                tr.appendChild(td);
+                tabla.appendChild(tr);
             }
+            document.getElementById("tablaUsuarios").appendChild(tabla);
         },
         error: function(xhr, status, error) {
             console.log(xhr + ", " + status + ", " + error);
